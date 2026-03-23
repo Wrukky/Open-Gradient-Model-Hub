5d3abeee5aed9b90102fc3df7f77f72930b5a83e
Wrukky <111442288+Wrukky@users.noreply.github.com>
2026-03-23 15:50:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,2 @@
-import { defineConfig } from ‘vite’
-import react from ‘@vitejs/plugin-react’
-
-export default defineConfig({
-plugins: [react()],
-// Change ‘og-model-hub’ to your actual GitHub repo name
-base: ‘/Open Gradient Model Hub/’,
-})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
